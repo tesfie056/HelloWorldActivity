e057000c606d4dc59c3ed41272f39c162eefbfb4
tesfie056 <143583774+tesfie056@users.noreply.github.com>
2023-09-14 15:25:55 -0400

    added constraint added event listener created properties assigned them to the layout

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -6,21 +6,29 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+abstract class MainActivity : AppCompatActivity() {
+
+
+
+
 
     // Declare view properties - the first one is done for you
-    lateinit var displayTextView: TextView
+    lateinit var displayTextView : TextView
+    lateinit var nameEditText : EditText
+    lateinit var clickMeButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+         setContentView(R.layout.activity_main)
 
         // Initialize with views defined in Layout - the first one is done for you
         displayTextView = findViewById(R.id.displayTextView)
+        nameEditText = findViewById(R.id.nameEditText)
+        clickMeButton = findViewById(R.id.clickMeButton)
 
         
         // Respond to button click event per specifications
-
+        clickMeButton.setOnClickListener{displayTextView.text = nameEditText.text}
 
     }
 }
